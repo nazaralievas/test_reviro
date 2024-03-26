72d93a3a8264f6f30b5c742ba379d3a550fd40c9
nazaralievas <sadanaza4@gmail.com>
2024-03-26 08:36:47 +0600

    pagination added

--- service/base/views.py
+++ service/base/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
 
 from .models import Product, Establishment
 from .serializers import ProductSerializer, EstablishmentSerializer
@@ -17,9 +18,16 @@ def apiOverview(request):
     return Response(api_urls)
 
 
+class CustomPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+
 class ProductList(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    pagination_class = CustomPagination
 
 
 class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -30,6 +38,7 @@ class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
 class EstablishmentList(generics.ListCreateAPIView):
     queryset = Establishment.objects.all()
     serializer_class = EstablishmentSerializer
+    pagination_class = CustomPagination
 
 
 class EstablishmentDetail(generics.RetrieveUpdateDestroyAPIView):
